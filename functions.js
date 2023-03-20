e90d9c793656f20b413f0b0f9473edac91e59701
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-03-20 19:52:36 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,77 +1,80 @@
-var localStorageBackup = function() {
-  var backup = {};
-  for (i = 0; i < localStorage.length; i++) {
-    var key = localStorage.key(i);
-    var value = localStorage.getItem(key);
-    backup[key] = escape(encodeURIComponent(value));
-  }
-  var json = JSON.stringify(backup);
-  var base = btoa(json);
-  var href = 'data:text/javascript;charset=utf-8;base64,' + base;
-  var link = document.createElement('a');
-    link.setAttribute('download', 'backup.json');
+function localStorageBackup() {
+  const backup = {};
+  Object.keys(localStorage).forEach((key) => {
+    const value = localStorage.getItem(key);
+    backup[key] = encodeURIComponent(value);
+  });
+  const json = JSON.stringify(backup);
+  const base = btoa(json);
+  const href = 'data:text/javascript;charset=utf-8;base64,' + base;
+  const link = document.createElement('a');
+  link.setAttribute('download', `backup_${new Date().toLocaleString()}.json`);
   link.setAttribute('href', href);
   document.querySelector('body').appendChild(link);
   link.click();
   link.remove();
-};
+}
 
-var localStorageRestore = function() {
-  var t = document.createElement('div');
-  var a = document.createElement('a');
-  a.appendChild(document.createTextNode('X'));
-  a.setAttribute('href', '#');
+function localStorageRestore() {
+  const div = document.createElement('div');
+  const anchor = document.createElement('a');
+  anchor.appendChild(document.createTextNode('X'));
+  anchor.setAttribute('href', '#');
 
-  a.style.position = 'absolute';
-  a.style.top = '10px';
-  a.style.right = '10px';
-  a.style['text-decoration'] = 'none';
-  a.style.color = '#fff';
-  t.appendChild(a);
-  a.onclick = function() {
-      t.remove();
+  anchor.style.position = 'absolute';
+  anchor.style.top = '10px';
+  anchor.style.right = '10px';
+  anchor.style['text-decoration'] = 'none';
+  anchor.style.color = '#fff';
+  div.appendChild(anchor);
+  anchor.onclick = function () {
+    div.remove();
   };
-  t.style.width = '50%';
-  t.style.position = 'absolute';
-  t.style.top = '25%';
-  t.style.left = '25%';
-  t.style['background-color'] = 'gray';
-  t.style['text-align'] = 'center';
-  t.style.padding = '50px';
-  t.style.color = '#fff';
-  t.style['z-index'] = 10000;
+  div.style.width = '50%';
+  div.style.position = 'absolute';
+  div.style.top = '25%';
+  div.style.left = '25%';
+  div.style['background-color'] = 'gray';
+  div.style['text-align'] = 'center';
+  div.style.padding = '50px';
+  div.style.color = '#fff';
+  div.style['z-index'] = 10000;
 
-  var l = document.createElement('input');
-  l.setAttribute('type', 'file');
-  l.setAttribute('id', 'fileinput');
-  l.onchange = function(e) {
-      t.remove();
-      var f = e.target.files[0];
-      if (f) {
-          var reader = new FileReader();
-          reader.onload = function(e) {
-              var text = e.target.result;
-              var backup = JSON.parse(text);
-              for (var key in backup){
-                 var value = decodeURIComponent(unescape(backup[key]));
-                 window.localStorage.setItem(key, value);
-               }
-              alert('Imported ' + Object.keys(backup).length + ' items from backup.')
-          };
-          reader.readAsText(f);
-      } else {
-        alert('Failed to load file');
-      }
+  const input = document.createElement('input');
+  input.setAttribute('type', 'file');
+  input.setAttribute('id', 'fileinput');
+  input.onchange = function (e) {
+    div.remove();
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const text = e.target.result;
+        const backup = JSON.parse(text);
+        for (const key in backup) {
+          const value = decodeURIComponent(backup[key]);
+          localStorage.setItem(key, value);
+        }
+        alert('Imported ' + Object.keys(backup).length + ' items from backup.');
+      };
+      reader.readAsText(file);
+    } else {
+      alert('Failed to load file');
+    }
   };
-  var a = document.createElement('h3');
-  a.appendChild(document.createTextNode('Select file with backup'));
-  t.appendChild(a);
-  t.appendChild(l);
-  document.querySelector('body').appendChild(t);
-};
+  const h3 = document.createElement('h3');
+  h3.appendChild(document.createTextNode('Select file with backup'));
+  div.appendChild(h3);
+  div.appendChild(input);
+  document.querySelector('body').appendChild(div);
+}
 
-var localStorageClear = function() {
-  if(window.confirm('Do you really want to delete all ' + localStorage.length + ' localStorage items of this website?')) {
+function localStorageClear() {
+  if (
+    window.confirm(
+      `Do you really want to delete all ${localStorage.length} localStorage items of this website?`,
+    )
+  ) {
     localStorage.clear();
   }
 }
